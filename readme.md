65c9dd24be633e8b4e631e3b6d64dc0e9628afc7
BaamPark <baam.park.95@gmail.com>
2025-03-20 18:44:45 -0400

    add readme and labelmat.py

---
+++ readme.md
@@ -0,0 +1,19 @@
+### 1. Prepare prerequisite
+
+``` bash
+data
+├── jx_vit_base_p16_224-80ecf9dd.pth
+├── label_mat
+│   ├── train_labels.pkl
+│   └── val_labels.pkl
+├── labels
+│   ├── attribute_label_split_train_0303_sgIsEa.json
+│   └── attribute_label_test_0303_sgIsEa.json
+├── ViT-B-16.pt
+└── ViT-L-14.pt
+```
+- Pre-trained CLIP, `ViT-L-14.pt` and `ViT-L-14.pt` will be downloaded automatically.
+- Download Pre-trained ViT through https://github.com/rwightman/pytorch-image-models/releases/download/v0.1-vitjx/jx_vit_base_p16_224-80ecf9dd.pth
+- download json
+- run `python tools/make_labelmat.py` to get pkl
+- 
---
+++ tools/make_labelmat.py
@@ -0,0 +1,31 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from dataset.PPEDataset import PPEmultilabelDataset, get_transform
+from tqdm import tqdm
+import pickle
+import torch
+
+train_tsfm, valid_tsfm = get_transform() 
+train_set = PPEmultilabelDataset(json_path="data/labels/attribute_label_split_train_0303_sgIsEa.json", transform=train_tsfm)
+valid_set = PPEmultilabelDataset(json_path="data/labels/attribute_label_test_0303_sgIsEa.json", transform=valid_tsfm) 
+
+
+train_labels = []
+for img, lbl in tqdm(train_set, desc="iterating trainset"):
+    train_labels.append(lbl)
+
+train_labels_tensor = torch.stack(train_labels)
+
+val_labels = []
+for img, lbl in tqdm(valid_set, desc="iterating trainset"):
+    val_labels.append(lbl)
+
+val_labels_tensor = torch.stack(val_labels)
+
+with open("data/label_mat/train_labels.pkl", "wb") as f:
+    pickle.dump(train_labels_tensor, f)
+
+with open("data/label_mat/val_labels.pkl", "wb") as f:
+    pickle.dump(val_labels_tensor, f)
