246841edc7f8543d8ad4c7f62638691b140ce5fb
BaamPark <baam.park.95@gmail.com>
2025-04-19 00:36:14 -0400

    correct fusion_len

--- models/MM_fusion_layer.py
+++ models/MM_fusion_layer.py
@@ -8,7 +8,7 @@ cfg = get_config()
 cfg_model = cfg['MODEL']
 cfg_clip = cfg['CLIP']
 
-class TransformerClassifier(nn.Module):
+class LinearProbe(nn.Module):
     def __init__(self, clip_model):
         super().__init__()
         self.attr_num = cfg_model['ATTRIBUTE_NUM']
@@ -23,12 +23,9 @@ class TransformerClassifier(nn.Module):
         self.use_region_split = cfg_clip['USE_REGION_SPLIT'] #bool
         self.use_mmformer = cfg_model['USE_MULTIMODAL_TRANSFORMER']
         self.fusion_len = 1 + 256 + self.attr_num + cfg_clip['REGION_SPLIT_NUM'] + cfg_clip['VISUAL_PROMPT_NUM']
+        self.fusion_len = 1 + 256 + self.attr_num + cfg_clip['REGION_SPLIT_NUM'] + cfg_clip['VISUAL_PROMPT_NUM']
+        self.linear_layer = nn.Linear(self.fusion_len, self.attr_num)
 
-        if not self.use_mmformer:
-            print('Without MM-former, Using MLP Instead')
-            self.linear_layer = nn.Linear(self.fusion_len, self.attr_num)
-        else:
-            self.blocks = vit.blocks[-cfg_model['MM_LAYERS_NUM']:]
 
     def forward(self,imgs,clip_model):
         b_s=imgs.shape[0]
@@ -41,13 +38,9 @@ class TransformerClassifier(nn.Module):
         textual_features = self.word_embed(text_features).expand(b_s, self.attr_num, self.dim)
         x = torch.cat([textual_features,clip_image_features], dim=1)
         
-        if self.use_mmformer:
-            for blk in self.blocks:
-                x = blk(x)
-        else :# using linear layer fusion
-            x = x.permute(0, 2, 1)
-            x= self.linear_layer(x)
-            x = x.permute(0, 2, 1)
+        x = x.permute(0, 2, 1)
+        x= self.linear_layer(x)
+        x = x.permute(0, 2, 1)
             
         x = self.norm(x)
         logits = torch.cat([self.weight_layer[i](x[:, i, :]) for i in range(self.attr_num)], dim=1)
--- models/build.py
+++ models/build.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 import torch
 import torch.nn as nn
 from models.promptpar import TransformerClassifier
+from models.MM_fusion_layer import LinearProbe
 from models.clip_utils import load
 from models.clip import build_clip
 from utils.config import get_config
@@ -21,7 +22,12 @@ def build_model():
             model = TransformerClassifier(clip_model=clip_model)
             
             # model.load_state_dict(checkpoint['model_state_dict'], strict=False)
-
+    elif cfg['MODEL']['NAME'] == 'linaer_probe':
+         clip_model, ViT_preprocess = load(name=cfg['CLIP']['PRETRAINED_ViT_NAME'], 
+                                              device='cuda',
+                                              download_root='data')
+         
+         model = LinearProbe(clip_model=clip_model)
     return model, clip_model
 
 
